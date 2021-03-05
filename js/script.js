202e2a0a834d24e0d68f07391aed97b8b2acb51b
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-05 14:56:53 +0100

    add. event on click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // 1 evento al click del pulsante calculate
 
+var tastoCalcolo = document.getElementById('calculate');
+
+tastoCalcolo.addEventListener("click",
+
+  function(){
+
+    alert("ciao");
+
+
+
+  }
+
+);
+
+
+
 // 2 verificare quali input radio sono fleggati
 
 // 3 sommare il valore degli input scelti
