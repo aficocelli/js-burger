826a74a835f2fcb1aa5317d96bb2dd1cb40741e7
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-05 16:30:21 +0100

    add. js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,62 @@
-// 1 evento al click del pulsante calculate
+// variabili definite
+var burgerName = document.getElementById('name-burger');
 
 var tastoCalcolo = document.getElementById('calculate');
 
-tastoCalcolo.addEventListener("click",
+var choice = document.getElementsByClassName('ingredients');
 
-  function(){
+var coupon = document.getElementById('coupon');
 
-    alert("ciao");
+var total = document.getElementById('total');
+
+var somma = 50;
 
 
+// array definiti
+
+var codeCoupon = ["burger20", "123", "sconto21"];
+
+// Evento al click del pulsante calculate
+
+tastoCalcolo.addEventListener ( "click",
+
+  function() {
+
+    // 1 verificare che sia stato inserito il nome burger
+    if (burgerName.value != ""){
+
+      // 2 verificare quali input radio sono fleggati
+
+      for ( var i = 0; i < choice.length; i++) {
+
+        if (choice[i].checked) {
+
+          // 3 sommare il valore degli input scelti
+
+          somma = somma + parseInt(choice[i].value);
+
+        }
+
+      }
+
+      // 4 verificare eventuale coupon
+
+      if(codeCoupon.includes(coupon.value)) {
+
+        somma = somma - ( somma * 0.20);
+
+        console.log(somma);
+      }
+
+      // 5 esitare il totolae
+      total.innerHTML = "$" + somma;
+
+    } else {
+
+      alert("Inserisci il nome del tuo burger");
+
+    }
 
   }
 
 );
-
-
-
-// 2 verificare quali input radio sono fleggati
-
-// 3 sommare il valore degli input scelti
-
-// 4 verificare eventuale coupon
-
-// 5 esitare il totolae
