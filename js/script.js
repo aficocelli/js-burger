28f2c0cf3fd64060d5b345c06174252f38cdf864
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-05 14:48:39 +0100

    add. js scheme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// 1 evento al click del pulsante calculate
 
+// 2 verificare quali input radio sono fleggati
+
+// 3 sommare il valore degli input scelti
+
+// 4 verificare eventuale coupon
+
+// 5 esitare il totolae
