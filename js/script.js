128e03e8bcd106e39910f286ac11a817706263a5
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-05 17:00:14 +0100

    add. layout css

--- js/script.js
+++ js/script.js
@@ -11,28 +11,23 @@ var total = document.getElementById('total');
 
 var somma = 50;
 
-
 // array definiti
-
 var codeCoupon = ["burger20", "123", "sconto21"];
 
 // Evento al click del pulsante calculate
-
 tastoCalcolo.addEventListener ( "click",
 
   function() {
 
     // 1 verificare che sia stato inserito il nome burger
-    if (burgerName.value != ""){
+    if (burgerName.value != "") {
 
       // 2 verificare quali input radio sono fleggati
-
-      for ( var i = 0; i < choice.length; i++) {
+      for (var i = 0; i < choice.length; i++) {
 
         if (choice[i].checked) {
 
           // 3 sommare il valore degli input scelti
-
           somma = somma + parseInt(choice[i].value);
 
         }
@@ -40,10 +35,9 @@ tastoCalcolo.addEventListener ( "click",
       }
 
       // 4 verificare eventuale coupon
-
       if(codeCoupon.includes(coupon.value)) {
 
-        somma = somma - ( somma * 0.20);
+        somma -= somma * 0.20;
 
         console.log(somma);
       }
